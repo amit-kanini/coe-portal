dc650a64bf8adf175c0af9549ba12b55aff4f68c
sugandh <sugandhjaynagar@gmail.com>
2022-01-18 20:45:21 +0530

    ViewDetails UI update

--- src/app/updatedetails/updatedetails.component.ts
+++ src/app/updatedetails/updatedetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-updatedetails',
@@ -8,6 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class UpdatedetailsComponent implements OnInit {
 
   constructor() { }
+
+  clicked=true;
+  ishidden=true;
+
+  public buttonChanges1() : void {
+    this.ishidden=false;
+    this.clicked=false;
+  }
+
+  public buttonChanges2() : void {
+    this.ishidden=true;
+    this.clicked=true;
+  }
+
+  public onSubmit() : void {
+   console.log("hello");
+  }
 
   ngOnInit(): void {
   }
